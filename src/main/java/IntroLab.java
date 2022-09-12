1e840dc0e0fd1d1c4e687a906be65941ed3294ee
Kevin Le <kevinle6231@gmail.com>
2022-09-12 13:41:52 -0400

    fixed error added closing bracket to zhoudav6 function.

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -54,6 +54,7 @@ public class IntroLab {
     public static String zhoudav6() {
         return printLabMessage("Hi", "David Zhou", true,
                 "Welcome to CSC207!");
+
     }
 
     public static String lekevi15(){
